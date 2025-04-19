533292a3905c4e21c196d11ac03979f8244fd502
marsodev <marceaud.pro@gmail.com>
2025-04-19 16:58:53 +0200

    rework of projects app

--- src/components/apps/ProjectsApp/ProjectsApp.jsx
+++ src/components/apps/ProjectsApp/ProjectsApp.jsx
@@ -1,25 +1,51 @@
 import React from "react";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import projet1 from "../../../assets/images/projet1.png";
 import IconButton from "../../ui/IconButton/IconButton";
 import "./ProjectsApp.css";
 
 const projects = [
   {
-    title: "Test",
-    description: "Test",
+    title: "Test 1",
+    description: "Petit projet de test n°1",
     link: "#",
+    image: projet1,
+    techs: ["faReact", "faJs", "faCss3Alt"],
   },
   {
-    title: "Test",
-    description: "Test",
+    title: "Test 1",
+    description: "Petit projet de test n°1",
     link: "#",
+    image: projet1,
+    techs: ["faReact", "faJs", "faCss3Alt"],
   },
   {
-    title: "Test",
-    description: "Test",
+    title: "Test 1",
+    description: "Petit projet de test n°1",
     link: "#",
+    image: projet1,
+    techs: ["faReact", "faJs", "faCss3Alt"],
   },
 ];
+
+import {
+  faReact,
+  faJs,
+  faCss3Alt,
+  faHtml5,
+  faNodeJs,
+  faPython,
+} from "@fortawesome/free-brands-svg-icons";
+
+const iconMap = {
+  faReact,
+  faJs,
+  faCss3Alt,
+  faHtml5,
+  faNodeJs,
+  faPython,
+};
 
 const ProjectsApp = ({ onBackHome }) => {
   return (
@@ -38,8 +64,24 @@ const ProjectsApp = ({ onBackHome }) => {
             rel="noopener noreferrer"
             className="project-card"
           >
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
+            <img
+              src={project.image}
+              alt={project.title}
+              className="project-image"
+            />
+            <div className="project-content">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <div className="project-techs">
+                {project.techs.map((tech, idx) => (
+                  <FontAwesomeIcon
+                    key={idx}
+                    icon={iconMap[tech]}
+                    className="tech-icon"
+                  />
+                ))}
+              </div>
+            </div>
           </a>
         ))}
       </div>
